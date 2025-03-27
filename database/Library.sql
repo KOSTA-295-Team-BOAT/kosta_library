aee8caa770417d04934e6b6aeb1c031dafbaeb16
taeyun <afds119@gmail.com>
2025-03-27 17:16:35 +0900

    라이브러리 테이블 생성 쿼리 추가
    도서관 테이블 쿼리

---
+++ database/Library.sql
@@ -0,0 +1,113 @@
+DROP DATABASE IF EXISTS library;
+CREATE DATABASE library;
+USE library;
+
+CREATE TABLE `book_rent` (
+	`rent_uid`	VARCHAR(25)	NOT NULL COMMENT 'Auto-Increment',
+	`user_id`	VARCHAR(25)	NOT NULL,
+	`rent_date`	DATETIME	NOT NULL DEFAULT NOW(),
+	`rent_status`	INT	NOT NULL DEFAULT 0 COMMENT '{0 = 대여 중, 1 = 반납완료, 2 =연체중, 3 = 연체반납}',
+	`rent_due`	DATETIME	NULL COMMENT '대여일로부터 {n}일 후 (비즈니스 로직에서 결정)',
+	PRIMARY KEY (`rent_uid`)
+);
+
+CREATE TABLE `course` (
+	`course_uid`	INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+	`course_name`	VARCHAR(25)	NOT NULL,
+	`course_open`	INT	NOT NULL DEFAULT 0 COMMENT '{0 = 닫힘, 1 = 신청 가능}',
+	`course_graduate_date`	DATETIME	NOT NULL,
+	PRIMARY KEY (`course_uid`)
+);
+
+CREATE TABLE `user` (
+	`user_id`	VARCHAR(25)	NOT NULL,
+	`user_password`	VARCHAR(128)	NOT NULL COMMENT '추후 암호화 고려해 해시값 최대길이 고려한 길이 설정',
+	`course_uid`	INT	NOT NULL,
+	`category_uid`	INT	NULL DEFAULT NULL COMMENT '초기값은 null, 회원가입 시 혹은 마이페이지에서 설정 가능',
+	`user_status`	INT	NOT NULL DEFAULT 0 COMMENT '{0 = 교육생, 1= 수료생}',
+	`user_score`	INT	NOT NULL DEFAULT 5 COMMENT '비즈니스 로직에서 결정',
+	PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `rent_detail` (
+	`rent_detail_uid`	INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+	`book_uid`	INT	NOT NULL,
+	`rent_uid`	VARCHAR(25)	NOT NULL,
+	`rent_return_state`	INT	NOT NULL DEFAULT 0 COMMENT '{0 = 미반납, 1 = 반납완료, 2 =연체중, 3 = 연체반납}',
+	`rent_return_due`	DATETIME	NULL,
+	PRIMARY KEY (`rent_detail_uid`)
+);
+
+CREATE TABLE `book_reservation` (
+	`reservation_uid`	INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+	`book_uid`	INT	NOT NULL,
+	`reservation_status`	INT	NOT NULL DEFAULT 0 COMMENT '{0 = 예약 중, 1 = 기간 만료, 2 = 정상 대여, 3 = 예약 취소}}',
+	`reservation_date`	DATETIME	NOT NULL DEFAULT NOW(),
+	`reservation_due`	DATETIME	NOT NULL COMMENT '예약일로부터 {n}일 후 (비즈니스 로직에서 결정)',
+	`user_id`	VARCHAR(25)	NOT NULL,
+	PRIMARY KEY (`reservation_uid`)
+);
+
+CREATE TABLE `book_category` (
+	`category_uid`	INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+	`category_name`	VARCHAR(25)	NOT NULL,
+	PRIMARY KEY (`category_uid`)
+);
+
+CREATE TABLE `book` (
+	`book_uid`	INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+	`book_name`	VARCHAR(50)	NOT NULL,
+	`book_author`	VARCHAR(50)	NOT NULL,
+	`book_publisher`	VARCHAR(50)	NOT NULL,
+	`category_uid`	INT	NOT NULL,
+	`category_uid2`	INT	NULL,
+	`category_uid3`	INT	NULL,
+	`book_store_date`	DATETIME	NOT NULL,
+	`book_status`	INT	NOT NULL DEFAULT 0 COMMENT '{0 = 대여 가능, 1 = 대여 중}',
+	PRIMARY KEY (`book_uid`)
+);
+
+CREATE TABLE `course_recommend` (
+	`course_recommend_uid`	INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+	`course_uid`	INT	NOT NULL,
+	`book_uid`	INT	NOT NULL,
+	PRIMARY KEY (`course_recommend_uid`)
+);
+
+-- 외래 키 제약 조건 설정
+
+ALTER TABLE `book_rent` ADD CONSTRAINT `FK_user_TO_book_rent_1`
+FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `user` ADD CONSTRAINT `FK_course_TO_user_1`
+FOREIGN KEY (`course_uid`) REFERENCES `course` (`course_uid`);
+
+ALTER TABLE `user` ADD CONSTRAINT `FK_book_category_TO_user_1`
+FOREIGN KEY (`category_uid`) REFERENCES `book_category` (`category_uid`);
+
+ALTER TABLE `rent_detail` ADD CONSTRAINT `FK_book_TO_rent_detail_1`
+FOREIGN KEY (`book_uid`) REFERENCES `book` (`book_uid`);
+
+ALTER TABLE `rent_detail` ADD CONSTRAINT `FK_book_rent_TO_rent_detail_1`
+FOREIGN KEY (`rent_uid`) REFERENCES `book_rent` (`rent_uid`);
+
+ALTER TABLE `book_reservation` ADD CONSTRAINT `FK_book_TO_book_reservation_1`
+FOREIGN KEY (`book_uid`) REFERENCES `book` (`book_uid`);
+
+ALTER TABLE `book_reservation` ADD CONSTRAINT `FK_user_TO_book_reservation_1`
+FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `book` ADD CONSTRAINT `FK_book_category_TO_book_1`
+FOREIGN KEY (`category_uid`) REFERENCES `book_category` (`category_uid`);
+
+ALTER TABLE `book` ADD CONSTRAINT `FK_book_category_TO_book_2`
+FOREIGN KEY (`category_uid2`) REFERENCES `book_category` (`category_uid`);
+
+ALTER TABLE `book` ADD CONSTRAINT `FK_book_category_TO_book_3`
+FOREIGN KEY (`category_uid3`) REFERENCES `book_category` (`category_uid`);
+
+ALTER TABLE `course_recommend` ADD CONSTRAINT `FK_course_TO_course_recommend_1`
+FOREIGN KEY (`course_uid`) REFERENCES `course` (`course_uid`);
+
+ALTER TABLE `course_recommend` ADD CONSTRAINT `FK_book_TO_course_recommend_1`
+FOREIGN KEY (`book_uid`) REFERENCES `book` (`book_uid`);
