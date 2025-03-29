d7bc846305f64564ef79c8b0d40630c6286bc79d
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-03-29 22:07:29 +0900

    daoImpl 파일 탬플릿 추가

---
+++ src/repository/dao/BookCategoryDaoImpl.java
@@ -0,0 +1,45 @@
+package repository.dao;
+
+import java.util.List;
+
+import business.dto.BookCategory;
+
+public class BookCategoryDaoImpl implements BookCategoryDao {
+
+	@Override
+	public void addCategory(BookCategory category) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public BookCategory getCategoryById(int categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BookCategory> getAllCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookCategory getCategoryByName(String categoryName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCategory(BookCategory category) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteCategory(int categoryId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/repository/dao/BookDaoImpl.java
@@ -0,0 +1,39 @@
+package repository.dao;
+
+import java.util.List;
+
+import business.dto.Book;
+
+public class BookDaoImpl implements BookDao {
+
+	@Override
+	public void addBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Book getBookById(int bookUid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteBook(int bookUid) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
