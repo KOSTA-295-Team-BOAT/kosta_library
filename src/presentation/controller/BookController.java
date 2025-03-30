dea0f0d911b3185e742cf7c96c5b79ea2d74bf47
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-03-31 03:30:15 +0900

    도서 대여 컨트롤러

--- src/presentation/controller/BookController.java
+++ src/presentation/controller/BookController.java
@@ -3,66 +3,89 @@ package presentation.controller;
 import java.sql.SQLException;
 import java.util.List;
 
-
-
 import business.dto.Book;
-import business.service.book.BookSearch;
+import business.dto.BookRent;
+import business.dto.User;
+import business.service.book.BookRentService;
+import business.service.book.BookSearchService;
 import exception.SearchWrongException;
 
-/** 
+/**
  * 기능별 상세 컨트롤러 : 도서 컨트롤러 클래스
+ * 
  * @author 박재현
  * @since 2025-03-29
  */
 public class BookController {
-	
-	public BookController(){
-		
+
+	public BookController() {
+
 	}
-	
-	public List<Book> getBookByBookName(String name) {					
-		BookSearch search = new BookSearch();
+
+	public List<Book> getBookByBookName(String name) {
+		BookSearchService search = new BookSearchService();
 		List<Book> returnBookList = null;
 		try {
 			returnBookList = search.searchBookByName(name);
 		} catch (SearchWrongException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			System.out.println("예외발생");
-			//TODO 뷰 연결
+			// TODO 뷰 연결
 		}
 		return returnBookList;
 	}
 
-	public List<Book> getBookByBookAuthor(String name) {					
-		BookSearch search = new BookSearch();
+	public List<Book> getBookByBookAuthor(String name) {
+		BookSearchService search = new BookSearchService();
 		List<Book> returnBookList = null;
 		try {
 			returnBookList = search.searchBookByAuthor(name);
 		} catch (SearchWrongException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			System.out.println("예외발생");
-			//TODO 뷰 연결
+			// TODO 뷰 연결
 		}
 		return returnBookList;
 	}
 
-	public List<Book> getBookByBookPublisher(String name) {					
-		BookSearch search = new BookSearch();
+	public List<Book> getBookByBookPublisher(String name) {
+		BookSearchService search = new BookSearchService();
 		List<Book> returnBookList = null;
 		try {
 			returnBookList = search.searchBookByPublisher(name);
 		} catch (SearchWrongException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			System.out.println("예외발생");
-			//TODO 뷰 연결
+			// TODO 뷰 연결
 		}
 		return returnBookList;
 	}
 
-	
-	
-	
+	public BookRent rentOneBook(User user, Book book) {
+		BookRentService service = new BookRentService();
+		BookRent rent = null;
+		try {
+			rent = service.rentOneBook(user, book);
+		} catch (Exception e) {
+			System.out.println("예외발생");
+			// TODO 뷰 연결
+		}
+		return rent;
+	}
+
+	public BookRent rentBooks(User user, List<Book> books) {
+		BookRentService service = new BookRentService();
+		BookRent rent = null;
+		try {
+			rent = service.rentBooks(user, books);
+		} catch (Exception e) {
+			System.out.println("예외발생");
+			// TODO 뷰 연결
+		}
+		return rent;
+	}
+
 }
