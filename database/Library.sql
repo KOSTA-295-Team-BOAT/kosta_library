32f3eacab8a6704a768620ca23c7e596d03c5561
taeyun <afds119@gmail.com>
2025-03-31 17:26:07 +0900

    쿼리 수정 찐찐 막

--- database/Library.sql
+++ database/Library.sql
@@ -2,51 +2,44 @@ DROP DATABASE IF EXISTS library;
 CREATE DATABASE library;
 USE library;
 
-CREATE TABLE `book_rent` (
-	`rent_uid`	INT	NOT NULL	COMMENT 'Auto-Increment',
-	`user_id`	VARCHAR(25)	NOT NULL,
-	`rent_date`	DATETIME	NOT NULL	DEFAULT NOW(),
-	`rent_status`	INT	NOT NULL	DEFAULT 0	COMMENT '{0 = 대여 중, 1 = 반납완료, 2 =연체중, 3 = 연체반납}',
-	`rent_due`	DATETIME	NULL	COMMENT '대여일로부터 {n}일 후 (비즈니스 로직에서 결정)'
+-- 2. 과정 테이블
+CREATE TABLE course (
+  course_uid INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+  course_name VARCHAR(25) NOT NULL,
+  course_open INT NOT NULL DEFAULT 0 COMMENT '{0 = 닫힘, 1 = 신청 가능}',
+  course_graduate_date DATETIME NOT NULL,
+  PRIMARY KEY (course_uid)
 );
 
-DROP TABLE IF EXISTS book_reservation;
-
-CREATE TABLE book_reservation (
-  reservation_uid INT NOT NULL AUTO_INCREMENT,
-  book_uid INT NOT NULL,
-  reservation_status INT NOT NULL DEFAULT 0,
-  reservation_date DATETIME NOT NULL DEFAULT NOW(),
-  reservation_due DATETIME NOT NULL,
+-- 3. 사용자 테이블
+CREATE TABLE user (
   user_id VARCHAR(25) NOT NULL,
-  PRIMARY KEY (reservation_uid)
+  user_password VARCHAR(128) NOT NULL COMMENT '추후 암호화 고려해 해시값 최대길이 고려한 길이 설정',
+  course_uid INT NOT NULL,
+  user_status INT NOT NULL DEFAULT 0 COMMENT '{0 = 교육생, 1 = 수료생}',
+  user_score INT NOT NULL DEFAULT 5 COMMENT '비즈니스 로직에서 결정',
+  user_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (user_id)
 );
 
-
-
-CREATE TABLE `course` (
-  `course_uid` INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
-  `course_name` VARCHAR(25) NOT NULL,
-  `course_open` INT NOT NULL DEFAULT 0 COMMENT '{0 = 닫힘, 1 = 신청 가능}',
-  `course_graduate_date` DATETIME NOT NULL,
-  PRIMARY KEY (`course_uid`)
-);
-
-
-CREATE TABLE `course_recommend` (
-	`course_recommend_uid`	INT	NOT NULL	COMMENT 'Auto-Increment',
-	`course_uid`	INT	NOT NULL,
-	`book_uid`	INT	NOT NULL
-);
-
-CREATE TABLE `book_category` (
-  category_uid INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
+-- 4. 도서 카테고리 테이블
+CREATE TABLE book_category (
+  category_uid INT NOT NULL AUTO_INCREMENT,
   category_name VARCHAR(25) NOT NULL,
   PRIMARY KEY (category_uid)
 );
 
-DROP TABLE IF EXISTS book;
+-- 5. 유저 관심 카테고리
+CREATE TABLE user_favorite (
+  user_favorite_id INT NOT NULL AUTO_INCREMENT,
+  user_id VARCHAR(25) NOT NULL,
+  category_uid INT NOT NULL,
+  PRIMARY KEY (user_favorite_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id),
+  FOREIGN KEY (category_uid) REFERENCES book_category(category_uid)
+);
 
+-- 6. 도서 테이블
 CREATE TABLE book (
   book_uid INT NOT NULL AUTO_INCREMENT,
   book_name VARCHAR(50) NOT NULL,
@@ -56,34 +49,47 @@ CREATE TABLE book (
   category_uid2 INT NULL,
   category_uid3 INT NULL,
   book_store_date DATETIME NOT NULL,
-  book_status INT NOT NULL DEFAULT 0 COMMENT '{0 = 대여 가능, 1 = 대여 중, 2 = 예약중}',
+  book_status INT NOT NULL DEFAULT 0 COMMENT '{0 = 대여 가능, 1 = 대여 중, 2 = 예약 중}',
   PRIMARY KEY (book_uid)
 );
 
-
-CREATE TABLE `rent_detail` (
-	`rent_detail_uid`	INT	NOT NULL	COMMENT 'Auto-Increment',
-	`book_uid`	INT	NOT NULL,
-	`rent_uid`	INT	NOT NULL,
-	`rent_return_state`	INT	NOT NULL	DEFAULT 0	COMMENT '{0 = 미반납, 1 = 반납완료, 2 =연체중, 3 = 연체반납}',
-	`rent_return_due`	DATETIME	NULL
+-- 7. 도서 대여 테이블
+CREATE TABLE book_rent (
+  rent_uid INT NOT NULL AUTO_INCREMENT,
+  user_id VARCHAR(25) NOT NULL,
+  rent_date DATETIME NOT NULL DEFAULT NOW(),
+  rent_status INT NOT NULL DEFAULT 0 COMMENT '{0 = 대여 중, 1 = 반납완료, 2 = 연체중, 3 = 연체반납}',
+  rent_due DATETIME NULL,
+  PRIMARY KEY (rent_uid)
 );
 
-CREATE TABLE `user` (
-	`user_id`	VARCHAR(25)	NOT NULL,
-	`user_password`	VARCHAR(128)	NOT NULL	COMMENT '추후 암호화 고려해 해시값 최대길이 고려한 길이 설정',
-	`course_uid`	INT	NOT NULL,
-	`user_status`	INT	NOT NULL	DEFAULT 0	COMMENT '{0 = 교육생, 1= 수료생}',
-	`user_score`	INT	NOT NULL	DEFAULT 5	COMMENT '비즈니스 로직에서 결정',
-    `user_name`  VARCHAR(50) NOT NULL
+-- 8. 대여 상세 테이블
+CREATE TABLE rent_detail (
+  rent_detail_uid INT NOT NULL AUTO_INCREMENT,
+  book_uid INT NOT NULL,
+  rent_uid INT NOT NULL,
+  rent_return_state INT NOT NULL DEFAULT 0 COMMENT '{0 = 미반납, 1 = 반납완료, 2 = 연체중, 3 = 연체반납}',
+  rent_return_due DATETIME NULL,
+  PRIMARY KEY (rent_detail_uid)
 );
 
+-- 9. 도서 예약 테이블
+CREATE TABLE book_reservation (
+  reservation_uid INT NOT NULL AUTO_INCREMENT,
+  book_uid INT NOT NULL,
+  reservation_status INT NOT NULL DEFAULT 0 COMMENT '{0 = 예약 중, 1 = 기간 만료, 2 = 정상 대여, 3 = 예약 취소}',
+  reservation_date DATETIME NOT NULL DEFAULT NOW(),
+  reservation_due DATETIME NOT NULL,
+  user_id VARCHAR(25) NOT NULL,
+  PRIMARY KEY (reservation_uid)
+);
 
-CREATE TABLE `user_favorite` (
-  `user_favorite_id` INT NOT NULL AUTO_INCREMENT COMMENT 'Auto-Increment',
-  `user_id` VARCHAR(25) NOT NULL,
-  `category_uid` INT NOT NULL COMMENT 'Auto-Increment',
-  PRIMARY KEY (`user_favorite_id`)
+-- 10. 과정별 추천 도서 테이블
+CREATE TABLE course_recommend (
+  course_recommend_uid INT NOT NULL AUTO_INCREMENT,
+  course_uid INT NOT NULL,
+  book_uid INT NOT NULL,
+  PRIMARY KEY (course_recommend_uid)
 );
 
 
