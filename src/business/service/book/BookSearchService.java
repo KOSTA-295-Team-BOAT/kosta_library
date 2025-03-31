b3b22d938ad894c95f528b57adb472e922f88a8c
Jaehyun Park <earlydreamer@users.noreply.github.com>
2025-03-31 16:52:14 +0900

    gpt가 멋대로 추가한 모드 주석처리

--- src/business/service/book/BookSearchService.java
+++ src/business/service/book/BookSearchService.java
@@ -77,19 +77,20 @@ public class BookSearchService {
 		return returnBookList;
 		
 	}
-	
-	// 신규 추가: 관심분야 코드에 따라 도서를 검색하며, 없는 경우 예외 발생
-	public List<Book> searchBookByInterest(String interestCode) throws SearchWrongException {
-		// 가정: bookCategoryDao.getCategoryByCode()가 null을 리턴하면 존재하지 않는 관심분야임
-		if(bookCategoryDao.getCategoryByCode(interestCode) == null) {
-			throw new SearchWrongException("없는 관심분야 입니다.");
-		}
-		try {
-			List<Book> returnBookList = bookDao.getBookByCategoryCode(interestCode); // 가정: 해당 메서드 존재
-			return returnBookList;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new SearchWrongException(e.getMessage());
-		}
-	}
+
+	//합의한 적 없는 기능을 멋대로 GPT가 추가하려고 함... 나중에 만들수도 있으니 일단 주석처리
+//	// 신규 추가: 관심분야 코드에 따라 도서를 검색하며, 없는 경우 예외 발생
+//	public List<Book> searchBookByInterest(String interestCode) throws SearchWrongException {
+//		// 가정: bookCategoryDao.getCategoryByCode()가 null을 리턴하면 존재하지 않는 관심분야임
+//		if(bookCategoryDao.getCategoryByCode(interestCode) == null) {
+//			throw new SearchWrongException("없는 관심분야 입니다.");
+//		}
+//		try {
+//			List<Book> returnBookList = bookDao.getBookByCategoryCode(interestCode); // 가정: 해당 메서드 존재
+//			return returnBookList;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			throw new SearchWrongException(e.getMessage());
+//		}
+//	}
 }
